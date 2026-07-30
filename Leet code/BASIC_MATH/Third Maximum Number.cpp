4047fa2be9a047ce53a12aee6c40c21c96be391c
M Adithya Bhat <adibhat2005@gmail.com>
2026-07-30 14:11:34 +0530

    Third max number

---
+++ Leet code/BASIC_MATH/Third Maximum Number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums)
+     {
+        
+     long long  int max1=LLONG_MIN;
+      long long int max2=LLONG_MIN;
+       long long int max3=LLONG_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==max1 || nums[i]==max2 || nums[i]==max3)
+            continue;
+
+            if(nums[i]>max1)
+           {
+            max3=max2;
+            max2=max1;
+            max1=nums[i];
+           }
+           else if(nums[i]>max2)
+           {
+            max3=max2;
+            max2=nums[i];
+           }
+           else if(nums[i]>max3)
+           max3=nums[i];
+        }
+       
+         if(max3 == LLONG_MIN)
+            return max1;
+
+        return max3;
+    }
+};
