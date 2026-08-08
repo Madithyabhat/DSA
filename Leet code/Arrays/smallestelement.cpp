080a22f0b1b889328f671a3cee49d477dfaac702
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-08 23:20:50 +0530

    add

--- Leet code/Arrays/smallestelement.cpp
+++ Leet code/Arrays/smallestelement.cpp
@@ -1,17 +1,17 @@
-class Solution {
-public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
+// class Solution {
+// public:
+//     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+//         int i = 0, j = 0;
 
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] < nums2[j])
-                i++;
-             else if (nums1[i] > nums2[j])
-                j++;
-            else
-                return nums1[i];
-        }
+//         while (i < nums1.size() && j < nums2.size()) {
+//             if (nums1[i] < nums2[j])
+//                 i++;
+//              else if (nums1[i] > nums2[j])
+//                 j++;
+//             else
+//                 return nums1[i];
+//         }
 
-        return -1;
-    }
-};
+//         return -1;
+//     }
+// };
