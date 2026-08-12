f048df47c11e497ca9fe1cd600401c6b4f0ee54f
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-12 21:12:51 +0530

    add

--- Leet code/Arrays/missingprefix.cpp
+++ Leet code/Arrays/missingprefix.cpp
@@ -10,3 +10,25 @@
 // Input: nums = [3,4,5,1,12,14,13]
 // Output: 15
 // Explanation: The longest sequential prefix of nums is [3,4,5] with a sum of 12. 12, 13, and 14 belong to the array while 15 does not. Therefore 15 is the smallest missing integer greater than or equal to the sum of the longest sequential prefix.
+
+// class Solution {
+// public:
+//     int missingInteger(vector<int>& nums)
+//      {
+//         int sum=nums[0];
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             if(nums[i]==nums[i-1]+1)
+//             sum=sum+nums[i];
+//             else
+//             break;
+//         }
+//         int x=sum;
+//         unordered_set<int> st(nums.begin(),nums.end());
+//         while(st.find(x)!=st.end())
+//         {
+//             x++;
+//         }
+//         return x;
+//     }
+// };
