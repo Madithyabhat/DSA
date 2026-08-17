be905ae104d1efeffc5e65ea8f1cbffdd92f85fb
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-17 19:09:22 +0530

    add

--- Leet code/Arrays/pascal.cpp
+++ Leet code/Arrays/pascal.cpp
@@ -16,14 +16,14 @@
 //         for(int i=0;i<numRows;i++)
 //         {
 //             vector<int>rows(i+1,1);
-            for(int j=1;j<i;j++)
-            {
-                rows[j]=ans[i-1][j-1]+ans[i-1][j];
-            }
-            ans.push_back(rows);
-        }
-        return ans;
+//             for(int j=1;j<i;j++)
+//             {
+//                 rows[j]=ans[i-1][j-1]+ans[i-1][j];
+//             }
+//             ans.push_back(rows);
+//         }
+//         return ans;
         
         
-    }
-};
+//     }
+// };
