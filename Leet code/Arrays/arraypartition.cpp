81fa99093efb2cc5e0583595c68d662233b5f8f1
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-18 23:19:21 +0530

    add

--- Leet code/Arrays/arraypartition.cpp
+++ Leet code/Arrays/arraypartition.cpp
@@ -15,11 +15,11 @@
 
 // class Solution {
 // public:
-    int arrayPairSum(vector<int>& nums) 
-    {
-        sort(nums.begin(), nums.end());
+    // int arrayPairSum(vector<int>& nums) 
+    // {
+    //     sort(nums.begin(), nums.end());
 
-        int sum = 0;
+    //     int sum = 0;
 
         for(int i = 0; i < nums.size(); i += 2)
         {
