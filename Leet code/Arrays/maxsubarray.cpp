111fdbd6e971372b840895da225c4043fd8c394e
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-23 13:21:10 +0530

    add

--- Leet code/Arrays/maxsubarray.cpp
+++ Leet code/Arrays/maxsubarray.cpp
@@ -1,18 +1,18 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
 
-        int currSum = 0;
-        int maxSum = INT_MIN;
+//         int currSum = 0;
+//         int maxSum = INT_MIN;
 
-        for(int num : nums) {
-            currSum += num;
-            maxSum = max(maxSum, currSum);
+//         for(int num : nums) {
+//             currSum += num;
+//             maxSum = max(maxSum, currSum);
 
-            if(currSum < 0)
-                currSum = 0;
-        }
+//             if(currSum < 0)
+//                 currSum = 0;
+//         }
 
-        return maxSum;
-    }
-};
+//         return maxSum;
+//     }
+// };
