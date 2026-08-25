65f6d7860325c34fc8dc3908df422d9ce421d589
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-25 23:49:49 +0530

    add

--- Leet code/Arrays/missingmultiple.cpp
+++ Leet code/Arrays/missingmultiple.cpp
@@ -1,14 +1,14 @@
-class Solution {
-public:
-    int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> st(nums.begin(), nums.end());
+// class Solution {
+// public:
+//     int missingMultiple(vector<int>& nums, int k) {
+//         unordered_set<int> st(nums.begin(), nums.end());
 
-        int x = k;
+//         int x = k;
 
-        while (st.count(x)) {
-            x += k;
-        }
+//         while (st.count(x)) {
+//             x += k;
+//         }
 
-        return x;
-    }
-};
+//         return x;
+//     }
+// };
