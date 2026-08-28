4381f64b269a23d3ff0c790e55cd2807055816e5
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-28 23:21:10 +0530

    add

--- Leet code/Arrays/binary search/2 value.cpp
+++ Leet code/Arrays/binary search/2 value.cpp
@@ -1,44 +1,44 @@
-class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first = -1, last = -1;
-        int beg = 0, end = nums.size() - 1;
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int first = -1, last = -1;
+//         int beg = 0, end = nums.size() - 1;
 
-        // Find first position
-        while (beg <= end) {
-            int mid = beg + (end - beg) / 2;
+//         // Find first position
+//         while (beg <= end) {
+//             int mid = beg + (end - beg) / 2;
 
-            if (nums[mid] == target) {
-                first = mid;
-                end = mid - 1; 
-            }
-            else if (nums[mid] < target) {
-                beg = mid + 1;
-            }
-            else {
-                end = mid - 1;
-            }
-        }
+//             if (nums[mid] == target) {
+//                 first = mid;
+//                 end = mid - 1; 
+//             }
+//             else if (nums[mid] < target) {
+//                 beg = mid + 1;
+//             }
+//             else {
+//                 end = mid - 1;
+//             }
+//         }
 
-        beg = 0;
-        end = nums.size() - 1;
+//         beg = 0;
+//         end = nums.size() - 1;
 
-        // Find last position
-        while (beg <= end) {
-            int mid = beg + (end - beg) / 2;
+//         // Find last position
+//         while (beg <= end) {
+//             int mid = beg + (end - beg) / 2;
 
-            if (nums[mid] == target) {
-                last = mid;
-                beg = mid + 1;
-            }
-            else if (nums[mid] < target) {
-                beg = mid + 1;
-            }
-            else {
-                end = mid - 1;
-            }
-        }
+//             if (nums[mid] == target) {
+//                 last = mid;
+//                 beg = mid + 1;
+//             }
+//             else if (nums[mid] < target) {
+//                 beg = mid + 1;
+//             }
+//             else {
+//                 end = mid - 1;
+//             }
+//         }
 
-        return {first, last};
-    }
-};
+//         return {first, last};
+//     }
+// };
