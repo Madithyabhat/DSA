f4f7ab2bf7a5a2299833095fd5a57c736cebde84
M Adithya Bhat <adibhat2005@gmail.com>
2026-08-28 23:24:15 +0530

    add

--- Leet code/Arrays/binary search/2 value.cpp
+++ Leet code/Arrays/binary search/2 value.cpp
@@ -1,12 +1,6 @@
 
 
-Testcase
-34. Find First and Last Position of Element in Sorted Array
-Solved
-Medium
-Topics
-premium lock icon
-Companies
+
 Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
 If target is not found in the array, return [-1, -1].
