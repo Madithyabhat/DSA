5335709a5d83d7a724449d0b044f8d669cdaee3e
M Adithya Bhat <adibhat2005@gmail.com>
2026-09-02 20:49:47 +0530

    add

--- Leet code/Arrays/montonic.cpp
+++ Leet code/Arrays/montonic.cpp
@@ -11,3 +11,27 @@
 // Example 3:
 // Input: nums = [1,3,2]
 // Output: false
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) 
+    {
+        int i=0;
+        int j=0;
+        int n=nums.size();
+        while(i<n-1 && nums[i]<=nums[i+1])
+        {
+           i++;
+        }
+         while(j<n-1 && nums[j]>=nums[j+1])
+        {
+           j++;
+        }
+        
+        if(i==n-1||j==n-1)
+        return true;
+        else
+        return false;
+        
+    }
+};
