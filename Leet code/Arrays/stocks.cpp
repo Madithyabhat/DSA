fe8276dff9576f69397e8ec33b112823017aafb4
M Adithya Bhat <adibhat2005@gmail.com>
2026-09-17 21:35:59 +0530

    add

---
+++ Leet code/Arrays/stocks.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+          int n=prices.size();
+        vector<int>best(n);
+        best[0]=prices[0];
+        int maxsum=0;
+     
+        for(int i=1;i<n;i++)
+        {
+            best[i]=min(best[i-1],prices[i-1]);
+         }
+         for(int i=0;i<n;i++)
+        {
+            int cursum=prices[i]-best[i];
+            maxsum =max(maxsum,cursum);
+         }
+
+         return maxsum;
+         
+
+
+        
+    }
+};
