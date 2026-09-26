c22a3ba6e850c83d8634ff92e1a954c9c4ab8abc
M Adithya Bhat <adibhat2005@gmail.com>
2026-09-26 20:30:10 +0530

    add

--- Leet code/Arrays/prefix.cpp
+++ Leet code/Arrays/prefix.cpp
@@ -1,19 +1,19 @@
-class Solution {
-public:
-    int pivotIndex(vector<int>& nums) {
-        int totalSum = 0;
-        for (int x : nums) totalSum += x;
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums) {
+//         int totalSum = 0;
+//         for (int x : nums) totalSum += x;
 
-        int leftSum = 0;
+//         int leftSum = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            int rightSum = totalSum - leftSum - nums[i];
+//         for (int i = 0; i < nums.size(); i++) {
+//             int rightSum = totalSum - leftSum - nums[i];
 
-            if (leftSum == rightSum) return i;
+//             if (leftSum == rightSum) return i;
 
-            leftSum += nums[i];
-        }
+//             leftSum += nums[i];
+//         }
 
-        return -1;
-    }
-};
+//         return -1;
+//     }
+// };
